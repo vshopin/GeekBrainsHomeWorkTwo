bdd05538ae067bf2a9dff8a5069e7503bf48341a
vshopin <shopin_va@mail.ru>
2023-01-30 14:06:10 +0700

    corrected the comments to the fifth commit

--- src/lesson_three/PhoneDirectory.java
+++ src/lesson_three/PhoneDirectory.java
@@ -16,20 +16,21 @@ public class PhoneDirectory {
     }
   }
 
+
   public static void main(String[] args) {
     var directory = new PhoneDirectory();
-    directory.phoneDirectory.put("89137775530", "Ivanov");
-    directory.phoneDirectory.put("89137775531", "Petrov");
-    directory.phoneDirectory.put("89137775532", "Sidorov");
-    directory.phoneDirectory.put("89137775533", "Govnov");
-    directory.phoneDirectory.put("89137775534", "Pidorov");
-    directory.phoneDirectory.put("89137775535", "Huidorov");
-    directory.phoneDirectory.put("89137775536", "Ivanov");
-    directory.phoneDirectory.put("89137775537", "Petrov");
+    directory.getPhoneDirectory().put("89137775529", "Ivanov");
+    directory.getPhoneDirectory().put("89137775530", "Ivanov");
+    directory.getPhoneDirectory().put("89137775531", "Petrov");
+    directory.getPhoneDirectory().put("89137775532", "Sidorov");
+    directory.getPhoneDirectory().put("89137775533", "Govnov");
+    directory.getPhoneDirectory().put("89137775534", "Pidorov");
+    directory.getPhoneDirectory().put("89137775535", "Huidorov");
+    directory.getPhoneDirectory().put("89137775536", "Ivanov");
+    directory.getPhoneDirectory().put("89137775537", "Petrov");
     directory.get("Ivanov");
     directory.get("Petrov");
     directory.get("Govnov");
-    System.out.println(directory.getPhoneDirectory());
   }
 
 }
